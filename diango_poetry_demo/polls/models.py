3880c56d0b1b95ad0e908fbe2d4a26f5abffae42
Ting Luo <shirleyayu@hotmail.com>
2022-06-11 23:25:27 +1000

    create model

--- diango_poetry_demo/polls/models.py
+++ diango_poetry_demo/polls/models.py
@@ -1,3 +1,14 @@
+from secrets import choice
 from django.db import models
+# import models module from django.db
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # if question is deleted, delete all the related choices
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
