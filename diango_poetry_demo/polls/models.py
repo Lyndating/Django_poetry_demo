92b03ea2d947d0eb59de051c55d69983b2981e3c
Ting Luo <shirleyayu@hotmail.com>
2022-06-12 15:03:38 +1000

    add __str__ custom method in polls/models

--- diango_poetry_demo/polls/models.py
+++ diango_poetry_demo/polls/models.py
@@ -1,4 +1,6 @@
-from secrets import choice
+from datetime import datetime, timedelta
+
+from django.utils import timezone
 from django.db import models
 # import models module from django.db
 
@@ -6,9 +8,19 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # if question is deleted, delete all the related choices
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
