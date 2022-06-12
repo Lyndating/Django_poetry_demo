b80622e7c65e848b32497a2079ff524a4b1b638f
Ting Luo <shirleyayu@hotmail.com>
2022-06-12 17:06:07 +1000

    add views path to polls/urls.py

--- diango_poetry_demo/polls/urls.py
+++ diango_poetry_demo/polls/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
 from . import views
-# from current repo import view file
+# from current repo import views.py
 
 urlpatterns = [
+    # index view: /polls/
     path('', views.index, name='index'),
+    # single view: /polls/:id/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # single result view: /polls/:id/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # single vote view: /polls/:id/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
