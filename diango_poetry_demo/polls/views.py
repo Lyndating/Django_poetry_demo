de16578804417bd63e71b8ec59a490d1b6836562
Ting Luo <shirleyayu@hotmail.com>
2022-06-13 19:39:44 +1000

    add generic way to views.py

--- diango_poetry_demo/polls/views.py
+++ diango_poetry_demo/polls/views.py
@@ -1,39 +1,57 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+# from django.template import loader
 from django.urls import reverse
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # indices from 0 to 4, order by reverse way using "-" before the column name
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # indices from 0 to 4, order by reverse way using "-" before the column name
 
-    # template = loader.get_template('polls/index.html')
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    # load the template under html file and pass it a context dictionary. 
-    # return HttpResponse(template.render(context, request))
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     # load the template under html file and pass it a context dictionary. 
+#     # return HttpResponse(template.render(context, request))
     
-    return render(request, "polls/index.html", context)
+#     return render(request, "polls/index.html", context)
 
-# we need to map the view to a URL (URLconf)
+# # we need to map the view to a URL (URLconf)
 
-# a "type" of web apge that serves a specific funtion and has specific template.
-# they take an argument
-def detail(request, question_id):
-    #print (request): <WSGIRequest: GET '/polls/1/'>
-    # return HttpResponse("You are looking at question %s." % question_id)
-    # %s operator let you add value into python string. the value after the % sign will be replaced to that place.
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,"polls/detail.html", {"question": question})
+# # a "type" of web apge that serves a specific funtion and has specific template.
+# # they take an argument
+# def detail(request, question_id):
+#     #print (request): <WSGIRequest: GET '/polls/1/'>
+#     # return HttpResponse("You are looking at question %s." % question_id)
+#     # %s operator let you add value into python string. the value after the % sign will be replaced to that place.
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request,"polls/detail.html", {"question": question})
 
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {'question': question})
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name= 'latest_question_list'
+
+    def get_queryset(self):
+        # return the last five published questions. 
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
     try: 
