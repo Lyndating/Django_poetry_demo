e7596045f28ec73be3cfe3ff2e8f5360c036f421
Ting Luo <shirleyayu@hotmail.com>
2022-06-13 19:40:04 +1000

    add generic way in urls.py

--- diango_poetry_demo/polls/urls.py
+++ diango_poetry_demo/polls/urls.py
@@ -6,11 +6,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # index view: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # single view: /polls/:id/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # single result view: /polls/:id/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # single vote view: /polls/:id/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
