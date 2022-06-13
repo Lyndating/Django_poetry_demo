05fd1639f47bdd0c5efb8ff8a81154b49fd9443c
Ting Luo <shirleyayu@hotmail.com>
2022-06-13 21:57:05 +1000

    admin register models

--- diango_poetry_demo/polls/admin.py
+++ diango_poetry_demo/polls/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Choice
+# register Question model
 admin.site.register(Question)
+# register Choice model
 admin.site.register(Choice)
