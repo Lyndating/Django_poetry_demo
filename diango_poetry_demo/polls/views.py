c219c70358ea1fc5fb200ab8493a2948ab113243
Ting Luo <shirleyayu@hotmail.com>
2022-06-13 14:37:22 +1000

    update real version of vote func in views.py

--- diango_poetry_demo/polls/views.py
+++ diango_poetry_demo/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -34,4 +35,19 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try: 
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        # here, request is an HttpRequest object.
+    except(KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/details.html', {
+            'question' : question,
+            "error_message": "Your didn't select a choice."
+            })
+    else:
+        selected_choice.votes +=1 
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+        # this reverse() call will return a string like 'polls/:id/results/'
+    
