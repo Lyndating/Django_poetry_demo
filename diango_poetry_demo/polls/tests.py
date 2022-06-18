860e493fae1cbf919968696146c363c55ea29a7a
Ting Luo <shirleyayu@hotmail.com>
2022-06-18 21:36:30 +1000

    add DetailView testing in tests.py

--- diango_poetry_demo/polls/tests.py
+++ diango_poetry_demo/polls/tests.py
@@ -1,4 +1,4 @@
-import re
+
 from django.test import TestCase
 
 # Create your tests here.
@@ -29,7 +29,7 @@ class QuestionModelTests (TestCase):
 
 
 
-# testing IndexView 
+# testing ListView 
 # first create a shortcut function 
 def create_question(question_text, days):
     # create a question with given 'question_text' and published the given number of days offset to now
@@ -68,3 +68,17 @@ class QuestionIndexViewTests(TestCase):
         question2 = create_question(question_text="Past Question 2", days = -5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['latest_question_list'], [question2,question1])
+
+
+# testing a DetailView
+    def test_future_question(self):
+        future_question = create_question(question_text="Future Question", days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    
+    def test_past_question(self):
+        past_question = create_question(question_text='Past Question', days =-5)
+        response = self.client.get(reverse('polls/detail', args=past_question.id))
+        self.assertContains(response, past_question.question_text)
+
