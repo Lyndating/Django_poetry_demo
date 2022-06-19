c1012550b2bce48144906ee32da29d050a545a30
Ting Luo <shirleyayu@hotmail.com>
2022-06-19 22:53:32 +1000

    fix Choice model, no related_name needed, use distinct() instead

--- diango_poetry_demo/polls/models.py
+++ diango_poetry_demo/polls/models.py
@@ -2,13 +2,14 @@ from datetime import datetime, timedelta
 
 from django.utils import timezone
 from django.db import models
+
 # import models module from django.db
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    
+    pub_date = models.DateTimeField("date published")
+
     def __str__(self):
         return self.question_text
 
@@ -18,7 +19,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="question_choice")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # if question is deleted, delete all the related choices
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
