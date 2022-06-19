60d846534fa670a3584ee6e81f324041ffada63d
Ting Luo <shirleyayu@hotmail.com>
2022-06-19 23:49:48 +1000

    customize admin form and change list

--- diango_poetry_demo/polls/admin.py
+++ diango_poetry_demo/polls/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Choice
+
 # register Question model
-admin.site.register(Question)
-# register Choice model
-admin.site.register(Choice)
+
+# remove register() call for Choice model
+# Choice objects are edited on the Question admin page.
+
+
+# class ChoiceInline(admin.StackedInline): display all fields in lines
+class ChoiceInline(admin.TabularInline):  # displaying tabular way
+    model = Choice
+    extra = 3  # by default provide fields for 3 choices
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
